90615d3fe8a7e0661e8283dbb17589c972db0354
Priyanka Saigal <email": null>
2016-01-17 16:33:25 -0800

    Add CSS color and font. Tabs change color when active and display text when clicked.

--- jQuery-DOM.js
+++ jQuery-DOM.js
@@ -7,8 +7,10 @@
 $(document).ready(function() {
   $(".tabs a").click(function(event) {
     event.preventDefault();
-    $(this).parent().addClass("current");
-    $(this).parent().siblings().removeClass("current");
+    $(this).parent().addClass("active");
+    $(this).parent().siblings().removeClass("active");
     var tab = $(this).attr("href");
+    $(".info").not(tab).css("display", "none");
+    $(tab).fadeIn();
   });
 });
