f55068084687c7ce3409feac0b99d8f02c7578d0
Priyanka Saigal <email": null>
2016-01-20 22:00:14 -0800

    Add CSS

--- jQuery-DOM.js
+++ jQuery-DOM.js
@@ -1,9 +1,3 @@
-// $(document).ready(function() {
-//   $("#content").append( "<h1 id='restaurant-title'>Central Perk</h1>" );
-//   $("#content").append("<p>Coffee! As seen on the hit TV Show FRIENDS</p>");
-//   $("#content").append("<img src='centralperk.jpg'>");
-// });
-
 $(document).ready(function() {
   $(".tabs a").click(function(event) {
     event.preventDefault();
